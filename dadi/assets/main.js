e570d20418b241d1de51d9b5a6efaca528d682dc
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-03 14:11:53 +0200

    add: created variables

--- dadi/assets/main.js
+++ dadi/assets/main.js
@@ -1,1 +1,13 @@
 console.log("Funzioni?");
+
+// Preparazione
+/* Stabilire due variabili alle quali assegnare due numeri casuali da 1 a 6;
+Trovare il modo per stabilire quale dei due numeri casuali sia il maggiore;
+Dare come vincitore il giocatore (utente o pc) che ha ottenuto il numero più grande */
+
+let playerNumber = Math.floor(Math.random() * 6) + 1;
+let botNumber = Math.floor(Math.random() * 6) + 1;
+console.log(playerNumber);
+console.log(botNumber);
+
+
