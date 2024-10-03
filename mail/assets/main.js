93910bf4e789c064ee92bb3f73e87b50ecc6dfaf
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-03 14:29:12 +0200

    add: preparation mail

--- mail/assets/main.js
+++ mail/assets/main.js
@@ -1,1 +1,10 @@
 console.log("Funzioni?");
+
+//Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+//Preparazione
+/* Creare un array con un elenco di email, che saranno la lista di chi può accedere;
+Creare un prompt per ricevere l'email dell'utente;
+Trovare il modo di confrontare la stringa inserita con gli elementi dell'array;
+Fare un if block, che stampi i messaggi a seconda della corrispondenza o meno del dato inserito, in caso con un alert */
+
